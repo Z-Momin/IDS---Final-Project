7fb0c40926a3431ffd8ff4994305b88c9c6d01fe
Ebrahim Rasromani <ekr6072@nyu.edu>
2020-11-03 00:45:47 -0500

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -1,4 +1,3 @@
-import torch
 import gym
 import random
 import numpy as np 
@@ -26,8 +25,6 @@ def key_release(k, mod):
 
 if __name__ == "__main__":
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
     env_name = "CarRacing-v0"
     env = gym.make(env_name).unwrapped
     restart = False
