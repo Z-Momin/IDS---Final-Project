2befad3400b2b758e13086cb362b5ddb99672802
Ebrahim Rasromani <ekr6072@nyu.edu>
2020-11-07 05:10:39 -0500

    Add files via upload

--- data_collection.py
+++ data_collection.py
@@ -1,12 +1,18 @@
-import torch
+import os
 import gym
 import random
+import json 
+import gzip
+import pickle
 import numpy as np 
 
+from itertools import count
 from pyglet.window import key
+from datetime import datetime
 
 
-RENDER = True
+RECORD = True
+
 
 def key_press(k, mod):
     global restart, action
@@ -24,25 +30,98 @@ def key_release(k, mod):
     if k == key.UP: action[1]    = 0.0
     if k == key.DOWN: action[2]  = 0.0
 
+
+def store_data(data, filename='data.pkl.gzip', directory="./data"):
+    # save data
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    data_file = os.path.join(directory, filename)
+    f = gzip.open(data_file,'wb')
+    pickle.dump(data, f)
+
+
+def save_results(episode_rewards, filename="results_manually.json", directory="./results"):
+    if len(episode_rewards) == 0: return
+
+    # save results
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    results = dict()
+    results["number_episodes"] = len(episode_rewards)
+    results["episode_rewards"] = episode_rewards
+
+    results["mean_all_episodes"] = np.array(episode_rewards).mean()
+    results["std_all_episodes"] = np.array(episode_rewards).std()
+    
+    fname = os.path.join(directory, filename)
+    fh = open(fname, "w")
+    json.dump(results, fh)
+    print('... finished')
+
+
+def push_to_buffer(replay_buffer, state, action, next_state, reward, done):
+    replay_buffer["state"].append(state)
+    replay_buffer["action"].append(np.array(action))
+    replay_buffer["next_state"].append(next_state)
+    replay_buffer["reward"].append(reward)
+    replay_buffer["terminal"].append(done)
+
+
 if __name__ == "__main__":
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+    results_fn = "results_manually-%s.json" % datetime.now().strftime("%Y%m%d-%H%M%S")
+    data_fn = "data-%s.pkl.gzip" % datetime.now().strftime("%Y%m%d-%H%M%S")
+
     env_name = "CarRacing-v0"
     env = gym.make(env_name).unwrapped
     restart = False
 
-    state = env.reset()
-    env.render()
+    replay_buffer = {
+        "state": [],
+        "next_state": [],
+        "reward": [],
+        "action": [],
+        "terminal" : [],
+    }
+
+    env.reset()
     env.viewer.window.on_key_press = key_press
     env.viewer.window.on_key_release = key_release
 
-    action = np.array([0., 0., 0.])
+    action = np.array([0., 0., 0.], dtype=np.float32)
+    episode_rewards = []
+    iteration = 0
 
-    while True:
-        next_state, reward, done, info = env.step(action)
-        if RENDER: env.render()
-        state = next_state
-        if done or restart: break
+    for episdoe in count():
+
+        episode_reward = 0
+        state = env.reset()
+
+        while True:
+
+            next_state, reward, done, info = env.step(action)
+            episode_reward += reward
+            push_to_buffer(replay_buffer, state, action, next_state, reward, done)
+
+            state = next_state
+            iteration += 1
+
+            if iteration % 500 == 0 or done:
+                print("\n[ITERATION]: {}".format(iteration))
+                print("\n[EPISODE]: {}".format(episdoe))
+                print("[AVERAGE EPISODE REWARD]: " + "{:0.2f}".format(np.array(episode_rewards).mean() if len(episode_rewards) != 0 else episode_reward))
+
+            if RECORD and iteration % 100 == 0:
+                print('... saving data')
+                store_data(replay_buffer, data_fn, "./data")
+                save_results(episode_rewards, results_fn,"./results")
+
+            env.render()            
+            if done or restart: break
+            
+        if restart: break
+
+        episode_rewards.append(episode_reward)
 
     env.close()
